6655d47fd00624cf88e8556072dfe4fd4d4c733b
Nicole MacLean <nicole.mac0404@gmail.com>
2022-03-07 19:00:08 -0800

    sp

--- week_1/day_1/pig-latin.js
+++ week_1/day_1/pig-latin.js
@@ -3,13 +3,13 @@
 const args = process.argv.slice(2);
 
 const makePiggy = function(args) {
-  let piggySentance = "";
+  let piggySentence = "";
   for (let i = 0; i < args.length; i++) {
     for (let c = 1; c < args[i].length; c++) {
-      piggySentance += args[i][c];
+      piggySentence += args[i][c];
     }
-    piggySentance = piggySentance + (`${args[i][0]}ay `);
+    piggySentence = piggySentence + (`${args[i][0]}ay `);
   }
-  return piggySentance;
+  return piggySentence;
 };
 console.log(makePiggy(args));
